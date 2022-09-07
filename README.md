89df4b139eaa350e8d6f8a6cfb94d7ed57c0ab0a
Tetsu HARUYAMA <tetsu@T-MBP3.local>
2022-09-07 13:19:15 +0900

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# japanize-matplotlib-jlite
+test drive for JupyterLite
+
+* Based on [japanize-matplotlib](https://github.com/uehara1414/japanize-matplotlib)
---
+++ setup.py
@@ -0,0 +1,22 @@
+# encoding: utf8
+import glob
+from setuptools import find_packages, setup
+
+additional_files = []
+for filename in glob.iglob('./japanize_matplotlib-jlite/**', recursive=True):
+    additional_files.append(filename.replace('./japanize_matplotlib_jlite/', ''))
+
+
+setup(name='japanize-matplotlib-jlite',
+      version='0.1.0',
+      description='JupyterLiteでmatplotlibのフォント設定を自動で日本語化する',
+      author='spring-haru',
+      author_email='tetsu.yes@gmail.com',
+      url='https://github.com/uehara1414/japanize-matplotlib',
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      license='MIT License',
+      packages=find_packages(),
+      install_requires=['matplotlib'],
+      include_package_data=True,
+      package_data={'japanize_matplotlib': additional_files})
